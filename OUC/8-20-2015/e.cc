1a937622925b36fd2cbe8c98022361985227e6f8
Lyeec <hucxarch@gmail.com>
2015-08-21 09:59:06 +0800

    input change

--- OUC/8-20-2015/e.cc
+++ OUC/8-20-2015/e.cc
@@ -1,36 +1,35 @@
-#include <iostream>
+#include <stdio.h>
 #include <string.h>
-#include <string>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-
-#define MAXN 2000005
+#define N 1000010
+char t[2*N] = "$#";
+int p[2*N];
 
 inline int min ( int a , int b ) { return a < b ? a : b ; }
 
 int main(int argc, char const *argv[]) {
-	cin.sync_with_stdio(false);
-	for ( string ins; cin >> ins && ins!="END"; ) {
-		string s = "$#";
-		for ( int i = 0, len = ins.length(); i < len; ++ i ) {
-			s.push_back(ins[i]);
-			s.push_back('#');
+	while ( true ) {
+		int len = 2, pos = 0, ans = 0;
+		char c = getchar(), *ps = t + len ;
+		if( c == 'E' ) {
+			break;
+		} else {
+			do {
+				len += 2;
+				* ps ++ = c;
+				* ps ++ = '#';
+			} while( (c=getchar()) != '\n' );
 		}
-		int len = s.length(), pos = 0, nMax = 0, ans = 0;
-		static int p[MAXN];
+		*ps = '\0';
 		memset(p, 0x0, sizeof(p));
 		for ( int i = 0; i < len; ++ i ) {
-			if ( nMax > i ) {
-				p[i] = min(p[2*pos-i], nMax-i);
+			if ( pos+p[pos] > i ) {
+				p[i] = min(p[2*pos-i], pos+p[pos]-i);
 			} else {
 				p[i] = 1;
 			}
-			for ( ; i>=p[i] && i+p[i] < len && s[i+p[i]] == s[i-p[i]] ; ++p[i]);
-			if ( p[i] + i > nMax ) {
-				nMax = p[i] + i ;
+			for ( ; t[i+p[i]] == t[i-p[i]] ; ++p[i]);
+			if ( p[i] + i > pos+p[pos] ) {
 				pos = i ;
 			}
 			if ( p[i] > ans ) {
@@ -38,7 +37,7 @@ int main(int argc, char const *argv[]) {
 			}
 		}
 		static int cnt = 0;
-		cout << "Case " << ++cnt << ": " << ans-1 << endl;
+		printf("Case %d: %d\n", ++cnt, ans-1);
 	}
 	return 0;
 }
